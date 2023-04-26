6267a77c14dfe6056feb1581c0689e42e9a80264
timebetovr <timebetov@gmail.com>
2023-04-26 18:48:10 +0500

    Add Anagram

--- arrayOperations.js
+++ arrayOperations.js
@@ -44,6 +44,12 @@ const arr9 = [5, 8, 3, 9, 6, 2, 10, 7, -1, 4];
 
 maxAmin(arr9);
 
+// Checking for ANAGRAM
+const dec = "decimal";
+const med = "medical";
+
+isAnagram(dec, med);
+
 // METHODS … ------------------------------------------------------------------
 // Missing single number in sorted array by subtracting the summary of elements...
 function missNum(array) {
@@ -255,4 +261,29 @@ function maxAmin(array) {
 
   console.log("\nMETHOD: Traverse");
   console.log(`\tMax: ${max}\n\tMin: ${min}`);
+}
+
+// METHOD: To check is it Anagram
+function isAnagram(A, B) {
+  let Hash = new Array(26).fill(0);
+  let i, j;
+
+  for (i=0; i<A.length; i++) {
+    Hash[A.codePointAt(i) - 97]++;
+  }
+
+  console.log("\nIs it Anagram?");
+
+  for (j=0; j<B.length; j++) {
+    Hash[B.codePointAt(j) - 97]--;
+
+    if (Hash[B.codePointAt(j) - 97] < 0) {
+      console.log("\tNo, it is not!");
+      break;
+    }
+
+    if (j+1 == B.length) {
+      console.log("\tYes, it is!");
+    }
+  }
 }
